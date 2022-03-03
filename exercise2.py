dac6f34d8e427a0bf754a0697f076baf4845bc78
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-03-02 18:38:49 -0600

    exercise2

---
+++ exercise2.py
@@ -0,0 +1,51 @@
+# 1.Write a function called simple(). Assign a different message to 5 variables and print each message.
+def simple():
+    apple = 'red'
+    print(apple)
+    orange = 'fruit'
+    print(orange)
+    soccer = 'sport'
+    print(soccer)
+    book = 'learning'
+    print(book)
+    computer = 'HP'
+    print(computer)
+
+simple()
+
+# 2.Write a function called simple2(). Assign a message to a variable, then print out that variable. Change the message
+#  and assign it to the variable again, but after the first print statement. Print the second message.
+#  Do these steps 2 more times. You should have 4 messages assigned to the same variable and 4 print functions
+#  showing the results
+
+def simple2():
+    water = "water hydrates the body"
+    print(water)
+    water = "helps the body flush out toxins"
+    print(water)
+    water = "forms saliva and mucus"
+    print(water)
+    water = "cushions the brain and spinal cord"
+    print(water)
+simple2()
+
+# 3.Write a function called message that takes 1 argument. Inside that function, write a print function that takes
+# the argument
+def message(title):
+   """write a message"""
+   print(f"my last name is{title}")
+
+message('Trujillo')
+
+
+
+
+
+
+
+
+
+
+
+
+
