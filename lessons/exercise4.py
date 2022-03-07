0339f28cac1896c6fe1c00fae0837ff6904c7eb2
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-03-06 18:30:56 -0600

    exercise4

---
+++ lessons/exercise4.py
@@ -0,0 +1,60 @@
+#1  2-8
+
+print(4+ 4)
+print(12 - 4)
+print(2 * 4)
+print(16 / 2)
+
+2-9
+
+my_number = 12
+print("My favorite number is", my_number)
+
+2.
+
+def number_sets():
+
+
+    val_int1 = 456234
+    val_int2 = 68423791
+    val_int3 = 13794628
+    val_int4 = 96374
+
+    print(val_int1)
+    print (val_int2)
+    print (val_int3)
+    print (val_int4)
+
+number_sets()
+
+#3
+
+def value_numbers(value1, value2):
+
+    value1 = float(77)
+    value2 = int(71.5)
+
+    print(value1)
+    print(value2)
+value_numbers('77.0','71')
+
+#4
+
+movie = input('what is your favorite movie')
+seen = int(input('How many times have you seen it?'))
+
+message = 'My favorite movie is {0} and ive seen this movie {1} times '
+print(message.format(movie, seen))
+
+
+
+
+
+
+
+
+
+
+
+
+
