c6b86a5c816466f7d784a73eff57d85e006f76ca
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-03-08 17:21:49 -0600

    exercise 6

---
+++ lessons/exercise6.py
@@ -0,0 +1,112 @@
+# 1 5-3
+
+
+alien_color = 'green'
+if alien_color == 'green':
+    print('You get 5 points.')
+
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('You get 5 points.')
+
+#2 5-4
+
+alien_color = 'green'
+if alien_color == 'green':
+    print('You get 5 points.')
+
+else:
+    print('You get 10 points')
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('You get 5 points')
+else:
+    print('You just 10 points')
+
+
+#3 5-5
+
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('you get 5 points')
+elif alien_color == 'yellow':
+    print('you get 10 points')
+else:
+    print('you get 15 points')
+
+
+alien_color = 'green'
+if alien_color == 'green':
+    print('you get 5 points')
+elif alien_color == 'red':
+    print('you get 10 points')
+else:
+    print('you get 15 points')
+
+
+alien_color = 'red'
+if alien_color == 'yellow':
+    print('you get 5 points')
+elif alien_color == 'red':
+    print('you get 10 points')
+else:
+    print('you get 15 points')
+
+
+
+#4 5-6
+
+age = 21
+
+if age < 2:
+    print("you're a baby")
+elif age < 4:
+    print("you're a toddler")
+elif age < 13:
+    print("you're a kid")
+elif age < 20:
+    print("you're a teenager")
+elif age < 65:
+    print("you're an adult")
+else:
+    print("you're an elder")
+
+# 5
+
+def prob_5(arg1):
+  print(bool(arg1))
+
+
+prob_5(12)
+prob_5(1.2)
+prob_5(0)
+prob_5(0.4)
+prob_5(0.0)
+ # 12 = true , 1.2 = true , 0 = false, 0.4 = true, 0.0=false
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# arguement was
+
+
+
+
+
+
