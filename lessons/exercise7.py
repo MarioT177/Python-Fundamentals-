e28ce6ad99de25acdc9b97681aad0ed81c667435
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-03-10 10:26:56 -0600

    exercise7

---
+++ lessons/exercise7.py
@@ -0,0 +1,32 @@
+
+# 1 4-3
+
+for value in range(1, 20):
+    print(value)
+
+
+
+
+# 2 4-6
+
+for number in range(1, 20, 3):
+    print(number)
+
+# 3 4-7
+
+three = list(range(3, 31, 3))
+for number in three:
+    print(number)
+
+
+# 4 4-8
+
+cubes = []
+for number in range(1,11):
+    cube = number ** 3
+    cubes.append(cube)
+
+for cube in cubes:
+    print(cube)
+
+
