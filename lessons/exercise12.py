994f55d04c49d5ae764fd85c54e2c2a4cca6b240
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-03-15 19:53:00 -0500

    exercise12.py

---
+++ lessons/exercise12.py
@@ -0,0 +1,74 @@
+# 1 10-6: Addition
+
+try:
+    x = input("Give me a number: ")
+    x = int(x)
+
+    y = input("Give me another number: ")
+    y = int(y)
+except ValueError:
+    print("Sorry, I really needed a number.")
+else:
+    sum = x + y
+    print(f"The sum of {x} and {y} is {sum}.")
+
+
+
+# 2 10-7: Addition Calculator
+
+print("Enter 'q' at any time to quit.\n")
+
+while True:
+    try:
+        x = input("\nGive me a number: ")
+        if x == 'q':
+            break
+
+        x = int(x)
+
+        y = input("Give me another number: ")
+        if y == 'q':
+            break
+
+        y = int(y)
+
+    except ValueError:
+        print("Sorry, I really needed a number.")
+
+    else:
+        sum = x + y
+        print(f"The sum of {x} and {y} is {sum}.")
+
+
+# 3   10-8: Cats and Dogs
+
+
+filenames = ['cats.txt', 'dogs.txt']
+
+for filename in filenames:
+    print(f"\nReading file: {filename}")
+    try:
+        with open(filename) as f:
+            contents = f.read()
+            print(contents)
+    except FileNotFoundError:
+        print(f"  Sorry, {filename} don't exist")
+
+
+# 4 10-9: Silent Dogs and Cats
+
+filenames = ['cats.txt', 'dogs.txt']
+
+for filename in filenames:
+
+    try:
+        with open(filename) as f:
+            contents = f.read()
+
+    except FileNotFoundError:
+        pass
+
+    else:
+        print(f"\nReading file: {filename}")
+        print(contents)
+        
